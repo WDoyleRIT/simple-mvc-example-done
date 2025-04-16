aba0c4040221301cbc6ae1866f7f030842db8438
Will Doyle (RIT Student) <wbd6359@rit.edu>
2025-04-16 14:34:42 -0400

    Completed Assignment

---
+++ server/models/Dog.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+let dogModel = {};
+
+const dogSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+  },
+
+  breed: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+  },
+
+  age: {
+    type: Number,
+    min: 0,
+    required: true,
+  },
+
+  createdDate: {
+    type: Date,
+    default: Date.now,
+  },
+
+});
+
+dogModel = mongoose.model('Dog', dogSchema);
+
+module.exports = dogModel;
